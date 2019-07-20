8cae31a872d2438158b5af31a78a572ad1c575e5
Marcel Kralt <marcel.kralt@gmail.com>
2019-07-20 16:41:09 +0200

    Project setup

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+	configureWebpack: {
+		devtool: 'source-map'
+	},
+  pluginOptions: {
+    'style-resources-loader': {
+      patterns: [
+        'D:\\projects\\startupz\\src\\styles\\_global.scss'
+      ],
+      preProcessor: 'scss'
+    },
+    ghPages: {
+      message: 'Update Github Pages'
+    }
+  },
+	publicPath: process.env.nodeENV === 'production'
+		? '/startupz/'
+		: '/'
+}
